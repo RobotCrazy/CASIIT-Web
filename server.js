e3c5551e98ca8c6a6390ffed43a7ee3a91d44cb5
Upneet <captainus55@gmail.com>
2019-08-23 21:26:15 -0400

    Added login functionality

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
 const session = require('express-session');
 
 const app = express();
 
+require('./config/passport')(passport);
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose.connect(db, { useNewUrlParser: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+
 app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+
 app.use(
     session({
         secret: 'secret',
@@ -12,14 +31,25 @@ app.use(
     })
 );
 
-app.use(express.static('public'));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use(express.urlencoded({ extended: true }));
+app.use(flash());
+
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
 
 
 app.get('/', (req, res) => {
     res.render('index');
 });
+
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
 
 const PORT = process.env.PORT || 3000;
 
